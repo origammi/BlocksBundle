f475ad268cc44d6e0647ae163fe9a79a9d2af351
matejvelikonja <matej@velikonja.si>
2015-05-27 15:07:39 +0200

    edit test

--- Test/Functional/BasicTest.php
+++ Test/Functional/BasicTest.php
@@ -36,7 +36,35 @@ class BasicTest extends BaseTest
             'form[blocks][blocks][2][image][file]' => $this->getTestUploadImage(),
             'form[blocks][blocks][3][text]'        => __METHOD__,
             'form[blocks][blocks][4][caption]'     => __METHOD__,
-            'form[blocks][blocks][4][externalId]'  => __METHOD__,
+            'form[blocks][blocks][4][externalId]'  => 'jI-kpVh6e1U',
+        ]);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect(), $this->getErrorFromResponse());
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(1, $crawler->filter('.flash-success')->count(), 'Flash message not found.');
+    }
+
+    /**
+     * Tests a really simple edit operation.
+     *
+     * @depends testSimpleSaveOfPost
+     */
+    public function testSimpleEditOfPost()
+    {
+        $crawler = $this->client->request('GET', '/edit/1');
+        $form    = $crawler->selectButton('Save')->form();
+
+        $this->client->submit($form, [
+            'form[title]'                          => __METHOD__,
+            'form[blocks][blocks][0][text]'        => __METHOD__,
+            'form[blocks][blocks][1][text]'        => __METHOD__,
+            'form[blocks][blocks][2][caption]'     => __METHOD__,
+            'form[blocks][blocks][2][image][file]' => $this->getTestUploadImage(),
+            'form[blocks][blocks][3][text]'        => __METHOD__,
+            'form[blocks][blocks][4][caption]'     => __METHOD__,
+            'form[blocks][blocks][4][externalId]'  => 'dQw4w9WgXcQ',
         ]);
 
         $this->assertTrue($this->client->getResponse()->isRedirect(), $this->getErrorFromResponse());
